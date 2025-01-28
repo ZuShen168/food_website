1c58dcebec93da739875b6335d05daae6c67f834
Ng Zu Shen <ngzushen@gmail.com>
2025-01-28 17:17:24 +0800

    react form practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
-import Heading from './Heading';
-import Card from './Card';
+import "./App.css";
+import { useState } from "react";
+import { validateEmail } from "./utils";
+
+const PasswordErrorMessage = () => {
+  return (
+    <p className="FieldError">Password should have at least 8 characters</p>
+  );
+};
 
 function App() {
-  function handleClick() { 
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNum);
-    let userInput = prompt('type a number'); 
-    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
-  }
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [role, setRole] = useState("role");
 
-  return ( 
-    <div className="App"> 
-      This is the starting code for "Your first component" ungraded lab 
-      <Heading firstName="Zu Shen"/>
-      <Heading firstName="Jason" /> 
-      <Card h2="First card's h2" h3="First card's h3"/>
-      <Card h2="s card's h2" h3="s card's h3"/>
-      <Card h2="t card's h2" h3="t card's h3"/>
-      <div>
-        <h1>Task: Add a button and handle a click event</h1>
-        <button onClick={handleClick}>Guess the number between 1 and 3</button>
-      </div>
-    </div> 
-  ); 
-} 
+  const getIsFormValid = () => {
+    const valid = firstName.length != 0 && validateEmail(email) && password.value.length >= 8 && (role != "role") && email.length != 0;
+    return valid;
+  };
+
+  const clearForm = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword({value:"", isTouched:false});
+    setRole("role");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getIsFormValid();
+    alert("Account created!");
+    clearForm();
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>
+              First name <sup>*</sup>
+            </label>
+            <input value={firstName} onChange={ e => setFirstName(e.target.value) } placeholder="First name" />
+          </div>
+          <div className="Field">
+            <label>Last name</label>
+            <input value={lastName} onChange={e => setLastName(e.target.value)} placeholder="Last name" />
+          </div>
+          <div className="Field">
+            <label>
+              Email address <sup>*</sup>
+            </label>
+            <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email address" />
+          </div>
+          <div className="Field">
+            <label>
+              Password <sup>*</sup>
+            </label>
+            <input
+              type="password"
+              value={password.value}
+              onChange={e => setPassword({...password,value: e.target.value})}
+              placeholder="Password"
+              onBlur={() => setPassword({...password, isTouched:true}) }/>
+            { password.isTouched && password.value.length<8? (<PasswordErrorMessage/>) : null}
+          </div>
+          <div className="Field">
+            <label>
+              Role <sup>*</sup>
+            </label>
+            <select value={role} onChange={e => setRole(e.target.value)}>
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>
+            Create account
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
 
 export default App;
